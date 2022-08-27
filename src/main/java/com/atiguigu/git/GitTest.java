4b4eead2a74435ee38ce4c6e5e2afac9031007da
Zhao <Zhaosq@qq.com>
2022-08-27 16:30:52 +0800

    second commit

--- src/main/java/com/atiguigu/git/GitTest.java
+++ src/main/java/com/atiguigu/git/GitTest.java
@@ -11,5 +11,6 @@ import java.sql.SQLOutput;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!");
     }
 }
