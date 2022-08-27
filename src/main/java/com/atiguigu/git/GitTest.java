16ab23674c0ee2849ace3fdf8adb85caf8a6c159
Zhao <Zhaosq@qq.com>
2022-08-27 16:53:02 +0800

    master commit2

--- src/main/java/com/atiguigu/git/GitTest.java
+++ src/main/java/com/atiguigu/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git4!");
+        System.out.println("master test!");
     }
 }
